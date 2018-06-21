12538519cd1d9a906db2dbace3d039adc23e97ea
narcisa9 <narcisaangheloiu@gmail.com>
2018-06-21 16:27:26 +0300

    Edit & Delete Data

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -47,7 +47,6 @@ class PostsController extends Controller
     public function store(Request $request)
     {
         //
-        
         $this->validate($request , [
             'title'=>'required',
             'body'=>'required'
@@ -83,6 +82,10 @@ class PostsController extends Controller
     public function edit($id)
     {
         //
+        $post = Post::find($id);
+        //return view('posts.edit')->with('post',$post);
+        return view('posts.edit')->with('post',$post);
+
     }
 
     /**
@@ -95,6 +98,18 @@ class PostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $this->validate($request , [
+            'title'=>'required',
+            'body'=>'required'
+         ]); 
+        // update Post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success','Post Updated');
+
     }
 
     /**
@@ -106,5 +121,8 @@ class PostsController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success','Post Removed');
     }
 }
