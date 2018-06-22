bd10dab37f7e3998a364052410980c3dc02201fe
narcisa9 <narcisaangheloiu@gmail.com>
2018-06-23 00:16:06 +0300

    Model Relationships

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,27 @@
                 <div class="panel-body">
                     <a href="posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    @if(count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                                @foreach($posts as $post)
+                                <tr>
+                                    <th>{{ $post->title}}</th>
+                                    <th><a href="posts/{{$post->id}}/edit">Edit</a></th>
+                                    <th>
+                                        {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=>'POST', 'class' => 'pull-right'])!!}
+                                        {{Form::hidden('_method','DELETE')}}
+                                        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </th>
+                                </tr>
+                                @endforeach
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
